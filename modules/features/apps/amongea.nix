033a06d1a42547d6cedc2c867793602b123908ec
yanpla <me@yanpla.nl>
2026-08-19 16:34:40 +0000

    Update amongea.nix

--- modules/features/apps/amongea.nix
+++ modules/features/apps/amongea.nix
@@ -1,27 +1,28 @@
 {
-  # nginx reverse proxy for amongea.yanpla.nl, fronting a service on
-  # 127.0.0.1:10000 with ACME TLS.
-  den.aspects.amongea.nixos = {
-    services.nginx = {
-      enable = true;
-      recommendedProxySettings = true;
-      recommendedTlsSettings = true;
-      recommendedGzipSettings = true;
-      virtualHosts."amongea.yanpla.nl" = {
-        enableACME = true;
-        forceSSL = true;
-        locations."/".proxyPass = "http://127.0.0.1:10000";
+  den.aspects.amongea.nixos =
+    { inputs, ... }:
+    {
+      services.nginx = {
+        enable = true;
+        recommendedProxySettings = true;
+        recommendedTlsSettings = true;
+        recommendedGzipSettings = true;
+        virtualHosts."amongea.yanpla.nl" = {
+          enableACME = true;
+          forceSSL = true;
+          locations."/".proxyPass =
+            "http://${inputs.infra-private.hostNetworking.tunneler.address}:10000";
+        };
       };
-    };
 
-    security.acme = {
-      acceptTerms = true;
-      defaults.email = "me@yanpla.nl";
-    };
+      security.acme = {
+        acceptTerms = true;
+        defaults.email = "me@yanpla.nl";
+      };
 
-    networking.firewall.allowedTCPPorts = [
-      80
-      443
-    ];
-  };
+      networking.firewall.allowedTCPPorts = [
+        80
+        443
+      ];
+    };
 }
