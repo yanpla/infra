e0510d2cac5ade274aca40c9c671bad75c834474
yanpla <me@yanpla.nl>
2026-09-16 18:13:39 +0000

    rm unused input

--- flake.nix
+++ flake.nix
@@ -33,9 +33,6 @@
 
     t3-code-nix.url = "github:LisaScheers/t3-code-nix";
     t3-code-nix.inputs.nixpkgs.follows = "nixpkgs-master";
-
-    walgit.url = "github:tobi/walgit";
-    walgit.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
--- modules/hosts/alpha.nix
+++ modules/hosts/alpha.nix
@@ -1,8 +1,6 @@
 {
   den,
-  pkgs,
   inputs,
-  system,
   ...
 }:
 {
