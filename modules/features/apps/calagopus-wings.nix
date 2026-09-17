9da4ef80ee0da57473013087b90b508d5d7f5e8d
yanpla <me@yanpla.nl>
2026-09-17 21:10:55 +0000

    fix wings

--- modules/features/apps/calagopus-wings.nix
+++ modules/features/apps/calagopus-wings.nix
@@ -6,7 +6,7 @@
   den.aspects.calagopus-wings.nixos =
     { inputs, lib, pkgs, ... }:
     let
-      wings = inputs.calagopus-nix.packages.${pkgs.system}.wings;
+      wings = inputs.calagopus-nix.packages.${pkgs.system}.wings-nightly;
       configFile = "/etc/calagopus/config.yml";
     in
     {
